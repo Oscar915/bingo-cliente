0bddb9f4b9edef880f5bd5bbd4c8e6ecfbba1204
Oscar Ballesta <oscarballestach@gmail.com>
2023-02-16 19:57:19 -0500

    Editando card

--- src/app/shared/cardboard/cardboard.component.ts
+++ src/app/shared/cardboard/cardboard.component.ts
@@ -6,10 +6,119 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cardboard.component.scss']
 })
 export class CardboardComponent implements OnInit {
-
+  isClicked1 = false;
+  isClicked2 = false;
+  isClicked3 = false;
+  isClicked4 = false;
+  isClicked5 = false;
+  isClicked6 = false;
+  isClicked7 = false;
+  isClicked8 = false;
+  isClicked9 = false;
+  isClicked10 = false;
+  isClicked11 = false;
+  isClicked12 = false;
+  isClicked13 = false;
+  isClicked14 = false;
+  isClicked15 = false;
+  isClicked16 = false;
+  isClicked17 = false;
+  isClicked18 = false;
+  isClicked19 = false;
+  isClicked20 = false;
+  isClicked21 = false;
+  isClicked22 = false;
+  isClicked23 = false;
+  isClicked24 = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onCellClicked1(){
+    this.isClicked1 = !this.isClicked1;
+  }
+
+  onCellClicked2(){
+    this.isClicked2 = !this.isClicked2;
+  }
+
+  onCellClicked3(){
+    this.isClicked3 = !this.isClicked3;
+  }
+
+  onCellClicked4(){
+    this.isClicked4 = !this.isClicked4;
+  }
+
+  onCellClicked5(){
+    this.isClicked5 = !this.isClicked5;
+  }
+  onCellClicked6(){
+    this.isClicked6 = !this.isClicked6;
+  }
+
+  onCellClicked7(){
+    this.isClicked7 = !this.isClicked7;
+  }
+
+  onCellClicked8(){
+    this.isClicked8 = !this.isClicked8;
+  }
+  onCellClicked9(){
+    this.isClicked9 = !this.isClicked9;
+  }
+  onCellClicked10(){
+    this.isClicked10 = !this.isClicked10;
+  }
+  onCellClicked11(){
+    this.isClicked11 = !this.isClicked11;
+  }
+  onCellClicked12(){
+    this.isClicked12 = !this.isClicked12;
+  }
+  onCellClicked13(){
+    this.isClicked13 = !this.isClicked13;
+  }
+  onCellClicked14(){
+    this.isClicked14 = !this.isClicked14;
+  }
+  onCellClicked15(){
+    this.isClicked15 = !this.isClicked15;
+  }
+  onCellClicked16(){
+    this.isClicked16 = !this.isClicked16;
+  }
+
+  onCellClicked17(){
+    this.isClicked17 = !this.isClicked17;
+  }
+
+  onCellClicked18(){
+    this.isClicked18 = !this.isClicked18;
+  }
+
+  onCellClicked19(){
+    this.isClicked19 = !this.isClicked19;
+  }
+
+  onCellClicked20(){
+    this.isClicked20 = !this.isClicked20;
+  }
+
+  onCellClicked21(){
+    this.isClicked21 = !this.isClicked21;
+  }
+
+  onCellClicked22(){
+    this.isClicked22 = !this.isClicked22;
+  }
+
+  onCellClicked23(){
+    this.isClicked23 = !this.isClicked23;
+  }
+
+  onCellClicked24(){
+    this.isClicked24 = !this.isClicked24;
+  }
 }
